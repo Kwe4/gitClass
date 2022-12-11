c9373f48e34a8234d804b6353bdba5ba20114c82
David Sokefun <user@MACs-MacBook-Pro.local>
2022-12-11 16:07:31 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+var express = require('express');
+
+var app = express();
+
+// view engine setup
+app.set('view engine', 'ejs');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static('public'));
+
+app.listen(3000, ()=>{
+  console.log('Now Listening on Port 3000 !')
+})
+
+// 1.  Untracked ! 
+// 2.  Stage !
+// 3.  Commited ! 
